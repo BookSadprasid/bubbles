8ca89da91ba58a848792df064a539f0046237af7
Book Sadprasid <booksadprasid@gmail.com>
2020-07-08 21:49:00 -0300

    Create landing page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,61 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    
+    <div className="container">
+      
+      <div className="left">
+          <h1 className="greeting">
+          Hi Aaron
+          </h1>
+          <h3 className="timecount">
+          Total 5 hours 42 minutes of breathing done.
+          </h3>
+
+          <h2 className="today">
+          Today
+          </h2>
+          
+          <h4 className="excerciseName">
+          Breath stacking
+          </h4>
+          <div className="progressbar">
+            <span> 4 sessions left </span>
+            <div className="statusbar" style={{width: 116.43, background: '#FAE5A5'}}>
+            </div>
+          </div>
+
+          <h4 className="excerciseName">
+          PEP Therapy
+          </h4>
+          <div className="progressbar">
+            <span> 5 minutes left </span>
+            <div className="statusbar" style={{width: 140, background: '#ACA5FA'}}>
+            </div>
+          </div>
+
+          <div className="gameLinkContainer">
+          <a className="gameLink" href="#">
+              Collect more paint.
+          </a>
+          <a className="gameLink" href="#">
+              <span>The studio.</span>
+          </a>
+          </div>
+
+      </div>
+      <div className="right">
+      </div>
+      
+    </div>
+
+    
+      
+      {/* <header className="">
+        <div className="">
+        <img src="white-background.png" className="white-background" alt= "white-background" />
+        </div>
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,7 +71,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
